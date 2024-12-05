eed03a3543afbf10918adf64de8a668dec2441b2
Trwifon <trifon@mail.com>
2024-12-06 00:23:02 +0200

    feat: added whitenoise

--- FisherPoint/settings.py
+++ FisherPoint/settings.py
@@ -24,6 +24,8 @@ DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
+# CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+
 
 # Application definition
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Adjust according to your project structure
+
+# Optional: Whitenoise settings
+WHITENOISE_USE_FINDERS = True
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static/styles/',
